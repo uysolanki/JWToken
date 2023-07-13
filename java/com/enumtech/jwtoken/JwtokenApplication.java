3a581fc9a111620e06fa327775e295ec36f4ab18
uysolanki <92295731+uysolanki@users.noreply.github.com>
2023-07-13 10:32:53 +0530

    Add files via upload

---
+++ java/com/enumtech/jwtoken/JwtokenApplication.java
@@ -0,0 +1,13 @@
+package com.enumtech.jwtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtokenApplication.class, args);
+	}
+
+}
